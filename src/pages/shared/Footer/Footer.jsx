438e572d858887afdfb27880940019a2741ddac8
sayembinamir <sayembinamir912@gmail.com>
2023-05-03 23:41:04 +0600

    footer section create

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='text-center'>copyright - sayem</p>
+            <p className='text-center'>copyright </p>
         </div>
     );
 };
